f43b41ed0326ea6aaa3f38705ea85642ccecb47f
Anikova D <midday.s.u.n@yandex.ru>
2024-05-24 04:45:28 +0300

    last commit!

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,5 +1,32 @@
 function About() {
-    return <h1>Hello from About page</h1>;
+    return (
+        <div className="container center-align">
+            <div className="section">
+                <div className="col center-align">
+                    <div className="col s12 m6">
+                        <h2>SPA React Project</h2>
+                        <h4>Made by Anikova Darya</h4>
+                        <p>
+                            Если вы это читаете - хорошего вам дня и будьте по
+                            жизни как котик снизу ^_^
+                        </p>
+                        <a
+                            href="https://proglib.io/p/tvoe-pervoe-spa-na-react-osnovnye-koncepcii-i-razrabotka-2020-01-13"
+                            className="waves-effect waves-light btn"
+                        >
+                            Learn More
+                        </a>
+                        <br />
+                        <br />
+                        <img
+                            style={{ maxWidth: "100%", maxHeight: "400px" }}
+                            src="https://i.pinimg.com/564x/0d/11/6c/0d116c8e58fa9a039115ee55fa57027d.jpg"
+                        />
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export { About };
--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -1,5 +1,21 @@
 function Contact() {
-    return <h1>Hello from Contact page</h1>;
+    return (
+        <div className="container center-align">
+            <div className="section">
+                <div className="col">
+                    <div className="col s12 m6">
+                        <p style={{ fontSize: "34px", fontWeight: "500" }}>
+                            А зачем вам мои контакты? -.-
+                        </p>
+                        <img
+                            style={{ maxWidth: "100%", maxHeight: "400px" }}
+                            src="https://i.pinimg.com/564x/0a/72/1f/0a721f60b2f74ac225867159c7e28975.jpg"
+                        />
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export { Contact };
