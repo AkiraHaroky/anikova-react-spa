55bee9ff684d69ab97d9e5a51bd27b212ae167d4
Anikova D <midday.s.u.n@yandex.ru>
2024-05-24 04:46:33 +0300

    last commit!

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -3,7 +3,7 @@ function Footer() {
         <footer className="page-footer teal">
             <div className="footer-copyright">
                 <div className="container">
-                    ©{new Date().getFullYear()} Copyright text
+                    ©{new Date().getFullYear()} Anikova Darya
                     <a className="grey-text text-lighten-4 right" href="https://github.com/AkiraHaroky/anikova-react-spa">
                         Repository
                     </a>
